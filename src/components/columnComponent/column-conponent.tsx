9946ef1167a3e5e08b07b793378d99e97c3a20cc
spadarynjaALINA <alinagusarik@gmail.com>
2022-11-25 05:11:06 +0300

    add some styles for column component

--- src/components/columnComponent/column-conponent.tsx
+++ src/components/columnComponent/column-conponent.tsx
@@ -1,13 +1,26 @@
 import { Button, Card } from 'antd';
-import { EditOutlined, PlusOutlined } from '@ant-design/icons';
+import {
+  CheckCircleTwoTone,
+  PlusOutlined,
+  DeleteOutlined,
+  CloseCircleOutlined,
+} from '@ant-design/icons';
 import './column-component.less';
 import { Task } from '../task/task';
 import { t } from 'i18next';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useAppDispatch } from '../../hooks';
+import { CustomModal } from '../../features/modal/modal';
+import { CreateBoardForm } from '../createBoard';
+import Draggable from 'react-draggable';
+import Meta from 'antd/lib/card/Meta';
 export const ColumnComponent = (props: { props: { columnId: string; title: string } }) => {
   const dispatch = useAppDispatch();
+  const del = useRef(null as unknown as HTMLDivElement);
+  const title = useRef(null as unknown as HTMLDivElement);
+  const column = useRef(null as unknown as HTMLDivElement);
   const [open, setOpen] = useState(false);
+  const [edit, setEdit] = useState(false);
   const [openConfirm, setOpenConfirm] = useState(false);
   const handleCancel = () => {
     setOpen(false);
@@ -15,27 +28,50 @@ export const ColumnComponent = (props: { props: { columnId: string; title: strin
   const closeConfirm = () => {
     setOpenConfirm(false);
   };
-
+  const handleEdit = (e: React.SyntheticEvent) => {
+    if (!title.current.contains(e.target as Node)) setEdit(false);
+  };
+  const columnTitle = () => {
+    return (
+      <div onClick={() => setEdit(true)} ref={title}>
+        {edit ? (
+          <div>
+            <input type={'text'} value={props.props.title} className="column-title" />
+            <CheckCircleTwoTone twoToneColor="#52c41a" />
+            <CloseCircleOutlined twoToneColor="#eb2f96" />
+          </div>
+        ) : (
+          <p>{props.props.title}</p>
+        )}
+      </div>
+    );
+  };
   const showModal = () => {
     setOpen(true);
-    // dispatch({
-    //   type: 'currentData',
-    //   payload: {
-    //     props: 'task',
-    //     data: { title: props.props.title, description: props.props.description },
-    //   },
-    // });
   };
+
   return (
     <Card
+      ref={column}
+      onClick={handleEdit}
       className="column"
-      title={props.props.title}
+      title={columnTitle()}
       bordered={false}
-      extra={[<EditOutlined key="ed" />]}
-      style={{ maxHeight: '70vh' }}
+      extra={[<DeleteOutlined key="ed" />]}
+      style={{ maxHeight: '72vh' }}
+      hoverable={true}
       actions={[
         <Button key={'new'} onClick={showModal} type="primary" ghost>
           {t('newTask')} <PlusOutlined />
+          <CustomModal
+            key={'new-modal'}
+            open={open}
+            cancel={handleCancel}
+            footer={false}
+            title={'New Task'}
+          >
+            <CreateBoardForm cancel={handleCancel} data={{ title: '', description: '' }} />
+          </CustomModal>{' '}
         </Button>,
       ]}
     >
